50ca8016388a9d5c3eebfbfa080f287a262f807d
Md Ahsanul Islam <67047779+AhsanHimel@users.noreply.github.com>
2022-01-30 20:54:48 +0600

    corrected the function

--- packages_oldPC_to_newPC.R
+++ packages_oldPC_to_newPC.R
@@ -1,32 +1,42 @@
 
-packages_oldPC_to_newPC(do = "old", location = "installed_previously.csv", install = T){
-  if(do == "old"){
-    
+packages_oldPC_to_newPC <- function(
+  do = "old", 
+  # old: writes csv file containing old packages' details
+  # new: installs packages in new computer
+  location = "installed_previously.csv",
+  version = "same"
+  # same: to install the same versions
+  # update: to install the updated versions
+){
+  if (do == "old") {
     # get all installed package details in a data frame
-    installedPreviously <- as.data.frame(
-      installed.packages(), 
-      row.names = F
-    )
+    installedPreviously <- as.data.frame(installed.packages(),
+                                         row.names = F)
     
     # writing a csv that contains old computer's package details
-    write.csv(x = installedPreviously, 
-              file = paste0(location), 
+    write.csv(x = installedPreviously,
+              file = paste0(location),
               row.names = F)
   } else if (do == "new"){
-    # Read the csv file from old pc
+    # Read the csv file from old computer
     installedPreviously <- read.csv(location)
     
     # get all installed package details in a data frame
-    installedNow <- as.data.frame(  
-      installed.packages(),
-      row.names = F
-    )
+    installedNow <- as.data.frame(installed.packages(),
+                                  row.names = F)
+    # finds packages that are not installed in the new computer
+    toInstall <- setdiff(installedPreviously$Package,
+                         installedNow$Package)
     
-    if(install = T){
-      
-      # finds packages that are not installed in the new pc
-      toInstall <- setdiff(installedPreviously$Package, installedNow$Package)
-      
+    if (version == "same") {
+      # to install the same versions
+      for (i in toInstall) {
+        ver = installedPreviously[installedPreviously$Package == i, "Version"]
+        message("(^_^) Installing ", i, " Version = ", ver)
+        if(!require(devtools)) install.packages(devtools)
+        devtools::install_version(package = i, version = ver)
+      }
+    } else if (version == "update") {
       # installs the packages that are not installed
       install.packages(toInstall)
     }
@@ -34,17 +44,6 @@ packages_oldPC_to_newPC(do = "old", location = "installed_previously.csv", insta
 }
 
 
-
-
-
-
-
-# to install the same versions
-for(i in toInstall) {
-  ver = installedPreviously[installedPreviously$Package == i, "Version"]
-  message("(^_^) Installing ",i," Version = ",ver)
-  devtools::install_version(package = i, version = ver)
-}
 
 
 commonPackages <- merge(installedNow[,c("Package","Version")], 
